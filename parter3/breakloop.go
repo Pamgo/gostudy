b0debc4b09a4761aef78b75285d06ec0d4f150c4
pamgo <pamgogdpyc@163.com>
2019-05-01 10:22:27 +0800

    循环跳出

--- parter3/breakloop.go
+++ parter3/breakloop.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+// 跳出指定循环
+func main()  {
+
+	OuterLoop:
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 5; j++ {
+			switch j {
+			case 2:
+				fmt.Println(i,j)
+				break OuterLoop
+			case 3:
+				fmt.Println(i,j)
+				break OuterLoop
+			}
+
+		}
+	}
+}
