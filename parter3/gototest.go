1276b6d6299b128c5d1ee516495555d53160c036
pamgo <pamgogdpyc@163.com>
2019-05-01 10:13:38 +0800

    使用goto跳出循环以及处理统一异常跳转

--- parter3/gototest.go
+++ parter3/gototest.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+
+	for i := 0; i < 10; i++ {
+		for y := 0; y < 10; y++ {
+			if y == 2 {
+				fmt.Println(y)
+				goto breakhere
+			}
+			fmt.Println(y)
+		}
+	}
+
+	// 手动返回，避免执行进入标签
+	return
+
+	breakhere:
+		fmt.Println("done")
+}
