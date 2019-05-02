d1b61912063269edc14860f4b1f80b669d353ddd
pamgo <pamgogdpyc@163.com>
2019-05-02 14:27:35 +0800

    可变参数

--- parter5/variableparam.go
+++ parter5/variableparam.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func joinStrings(slist ...string) string  {
+
+	var b bytes.Buffer
+
+	for _,s := range slist  {
+		b.WriteString(s)
+	}
+
+	return b.String()
+}
+
+func main()  {
+	fmt.Println(joinStrings("alison","pamgo"))
+}
