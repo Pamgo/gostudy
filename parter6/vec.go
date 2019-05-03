4f49626ed72c0db4cef46c1aecbb1ea8bb5f7143
pamgo <pamgogdpyc@163.com>
2019-05-03 12:58:11 +0800

    二维矢量结构

--- parter6/vec.go
+++ parter6/vec.go
@@ -1,1 +1,41 @@
 package main
+
+import "math"
+
+type Vec2 struct {
+	X, Y float32
+}
+
+// 使用矢量加上另一个矢量，生成新的矢量
+func (v Vec2) Add(other Vec2) Vec2 {
+	return Vec2{
+		v.X + other.X,
+		v.Y + other.Y,
+	}
+}
+
+// 使用矢量乘以另外一个矢量，生产新的矢量
+func (v Vec2) Scale(s float32) Vec2 {
+	return Vec2{
+		v.X * s,
+		v.Y * s,
+	}
+}
+
+// 计算两个矢量的距离
+func (v Vec2) Distance(other Vec2) float32 {
+	dx := v.X - other.X
+	dy := v.Y - other.Y
+
+	return float32(math.Sqrt(float64(dx*dx + dy*dy)))
+}
+
+// 返回当前矢量的标准化矢量
+func (v Vec2) Normalize() Vec2 {
+	mag := v.X*v.X + v.Y*v.Y
+	if mag > 0 {
+		oneOverMag := 1 / float32(math.Sqrt(float64(mag)))
+		return Vec2{v.X * oneOverMag, v.Y * oneOverMag}
+	}
+	return Vec2{0,0}
+}
