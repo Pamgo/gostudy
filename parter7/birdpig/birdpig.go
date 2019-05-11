f147e5ece7ff7b65b03d7a14226b8950e3de9aa9
pamgo <pamgogdpyc@163.com>
2019-05-11 12:09:01 +0800

    接口嵌套

--- parter7/birdpig/birdpig.go
+++ parter7/birdpig/birdpig.go
@@ -1,1 +1,56 @@
-package birdpig
+package main
+
+import "fmt"
+
+// 定义飞行动物接口
+type Flyer interface {
+	Fly()
+}
+// 定义行走动物接口
+type Walker interface {
+	Walk()
+}
+// 定义鸟类
+type bird struct {
+
+}
+
+func (b *bird) Fly()  {
+	fmt.Println("bird: fly")
+}
+// 为鸟添加行走方法
+func (b *bird) Walk()  {
+	fmt.Println("bird: walk")
+}
+// 定义猪
+type pig struct {
+
+}
+
+func (p *pig) Walk()  {
+	fmt.Println("pig: walk")
+}
+
+func main()  {
+	animals := map[string]interface{} {
+		"bird": new(bird),
+		"pig": new(pig),
+	}
+
+	// 遍历映射
+	for name, obj := range animals{
+		// 判断对象是否为飞行动物
+		f, isFly := obj.(Flyer)
+		// 判断对象是否为行走动物
+		w, isWalker := obj.(Walker)
+
+		fmt.Printf("name:%s isFlayer: %v isWalker:%v\n",name,isFly,isWalker)
+
+		if isFly {
+			f.Fly()
+		}
+		if isWalker {
+			w.Walk()
+		}
+	}
+}
--- parter7/innerInter.go
+++ parter7/innerInter.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// 定义一个设备
+type device struct {
+
+}
+// 实现io.Writer的Write()方法
+func (d *device) Write(p []byte) (n int, err error) {
+	fmt.Println(string(p))
+	return 0,nil
+}
+func (d *device) Close()  error {
+	return nil
+}
+
+func main()  {
+	// 声明写入关闭器，并赋予device的实例
+	var wc io.WriteCloser = new(device)
+
+	// 写入数据
+	wc.Write(nil)
+
+	// 关闭设备
+	wc.Close()
+
+	// 声明写入器，并赋予device的新实例
+	var writeOnly io.Writer = new(device)
+
+	// 写入数据
+	writeOnly.Write(nil)
+}
