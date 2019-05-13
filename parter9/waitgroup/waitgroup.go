19900e7033d811f81204e3c588677469c7e1235e
pamgo <pamgogdpyc@163.com>
2019-05-13 21:28:04 +0800

    等待组（sync.WaitGroup)，保证在并发环境中完成指定数量的任务

--- parter9/waitgroup/waitgroup.go
+++ parter9/waitgroup/waitgroup.go
@@ -1,1 +1,40 @@
-package waitgroup
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+func main()  {
+
+	// 声明一个等待组
+	var wg sync.WaitGroup
+
+	// 准备一些列网址
+	var urls = []string{
+		"http://www.github.com/",
+		"http://www.baidu.com/",
+		"https://www.golangtc.com/",
+	}
+
+	for _, url := range urls{
+		// 每一个任务开始时，将等待组增加1
+		wg.Add(1)
+
+		go func(url string) {
+
+			// 使用defer，表示函数完成时将等待组值减1
+			defer wg.Done()
+
+			// 使用http访问提供的地址
+			_, err := http.Get(url)
+
+			fmt.Println(url, err)
+		}(url)
+
+		wg.Wait()
+
+		fmt.Println("over")
+	}
+}
