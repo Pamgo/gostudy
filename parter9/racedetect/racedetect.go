8f425f61f538b50d745ba5261ba228179184223b
pamgo <pamgogdpyc@163.com>
2019-05-13 21:16:56 +0800

    原子性atomic/sync.Mutex/SYNC.RWMutex

--- parter9/racedetect/racedetect.go
+++ parter9/racedetect/racedetect.go
@@ -1,1 +1,25 @@
-package racedetect
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+var (
+	seq int64
+)
+
+func GenID() int64 {
+
+	return atomic.AddInt64(&seq, 1)
+
+	//return seq
+}
+
+func main()  {
+	for i := 0; i < 10; i++ {
+		go GenID()
+	}
+
+	fmt.Println(GenID())
+}
--- parter9/racedetect/syncMutex.go
+++ parter9/racedetect/syncMutex.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	// 逻辑中使用的某个变量
+	count int
+	// 与变量对应的使用互斥锁
+	//countGuard sync.Mutex
+	countGuard sync.RWMutex
+)
+
+func GetCount() int {
+	countGuard.Lock()
+
+	defer countGuard.Unlock()
+
+	return count
+}
+
+func SetCount(c int)  {
+	countGuard.Lock()
+	count = c
+	countGuard.Unlock()
+}
+
+func GetCount2() int {
+	countGuard.Lock()
+	defer countGuard.Unlock()
+
+	return count
+}
+
+func main()  {
+	SetCount(1)
+
+	fmt.Println(GetCount2())
+}
